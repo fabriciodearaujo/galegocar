555f4cafac44e808462e60062117d94d40a0758f
fabriciodearaujo <fabricioaraujo4000@gmail.com>
2026-05-27 15:20:34 -0300

    add

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const SB_URL = 'https://uqkqsnutidzianefmzxs.supabase.co';
+const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxa3FzbnV0aWR6aWFuZWZtenhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTc0MjksImV4cCI6MjA5NTM5MzQyOX0.OxLpfDbTfpwTR-WGHjKGtsqaz-fQAxtR8zQ8ANWz3vw';
+const db = supabase.createClient(SB_URL, SB_KEY);
